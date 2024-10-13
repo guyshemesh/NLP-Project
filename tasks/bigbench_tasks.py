2ad88f6d55c6fe1cd994f984235f4f79d0872d14
adishani1 <adishani1@mail.tau.ac.il>
2024-10-13 19:43:28 +0300

    add task files

--- tasks/bigbench_tasks.py
+++ tasks/bigbench_tasks.py
@@ -1,13 +1,12 @@
-import json
 import importlib
 import os
 
 import bigbench.api.util as util
 from bigbench.api import json_task
-import bigbench.models.huggingface_models as huggingface_models
 
 
 def get_task(dataset, number_json_examples, number_json_shots):
+    print("start get_task()")
     task_module_name = f"bigbench.benchmark_tasks.{dataset}"
     task_module = importlib.import_module(task_module_name)
     module_path = list(task_module.__path__)[0]
@@ -17,9 +16,12 @@ def get_task(dataset, number_json_examples, number_json_shots):
         task = json_task.JsonTask(json_path, shot_list=[number_json_shots], verbose=False, max_examples=number_json_examples)
     else:
         task = util.load_programmatic_task(task_submodule_name)
+    print("end get_task()")
     return task
 
 
 def evaluate_model_task(model, task):
+    print("start evaluate")
     score_data = task.evaluate_model(model)
+    print("end evaluate")
     return score_data
--- tasks/run_task.py
+++ tasks/run_task.py
@@ -1,18 +1,25 @@
 import bigbench_tasks
+import pythia_model
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
-def load_model():
-    model_directory = "pythia/models/pythia-6.9b-v0/original_pythia/"
-    tokenizer_directory = "pythia/tokenizers/pythia-6.9b-v0/pythia_tokenizer/"
+def load_pythia_model():
+    print("start load_pythia_model()")
+    model_directory = "/home/joberant/NLP_2324b/adishani1/Pythia/models/pythia-6.9b-v0/original_pythia/"
+    tokenizer_directory = "/home/joberant/NLP_2324b/adishani1/Pythia/tokenizers/pythia-6.9b-v0/pythia_tokenizer/"
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_directory)
     model = AutoModelForCausalLM.from_pretrained(model_directory, torch_dtype=torch.float16)
-    return model
+    print("end load_pythia_model() and start PythiaModel")
+    return pythia_model.PythiaModel("original_pythia", model, tokenizer, "Original Pythia")
 
 
 def main():
+    print("start main()")
     task = bigbench_tasks.get_task(dataset="elementary_math_qa", number_json_examples=None, number_json_shots=2)
-    model = load_model()
+    model = load_pythia_model()
     score_data = bigbench_tasks.evaluate_model_task(model, task)
+    print("end main()")
     print(score_data)
 
 
