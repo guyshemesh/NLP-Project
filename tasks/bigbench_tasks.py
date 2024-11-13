a09ef51ac74cc2098d2f361fae4f7d81b267d689
adishani1 <116682946+adishani1@users.noreply.github.com>
2024-11-13 20:16:49 +0200

    Update bigbench_tasks.py

--- tasks/bigbench_tasks.py
+++ tasks/bigbench_tasks.py
@@ -6,7 +6,6 @@ from bigbench.api import json_task
 
 
 def get_task(dataset, number_json_examples, number_json_shots):
-    print("start get_task()")
     task_module_name = f"bigbench.benchmark_tasks.{dataset}"
     task_module = importlib.import_module(task_module_name)
     module_path = list(task_module.__path__)[0]
@@ -16,12 +15,9 @@ def get_task(dataset, number_json_examples, number_json_shots):
         task = json_task.JsonTask(json_path, shot_list=[number_json_shots], verbose=False, max_examples=number_json_examples)
     else:
         task = util.load_programmatic_task(task_submodule_name)
-    print("end get_task()")
     return task
 
 
 def evaluate_model_task(model, task):
-    print("start evaluate")
     score_data = task.evaluate_model(model)
-    print("end evaluate")
     return score_data
